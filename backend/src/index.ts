8254340b37fd562bfdab143e2f92ce3720411bf8
Hiro <skinner.daniel2004@gmail.com>
2025-02-26 08:52:53 -0700

    Replace with Mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,12 @@
 import { Application } from "@outwalk/firefly";
 import { ExpressPlatform } from "@outwalk/firefly/express";
-import { Database } from "@outwalk/firefly";
+import { MongooseDatabase } from "@outwalk/firefly/mongoose";
 
 /* setup the platform and global middleware */
 const platform = new ExpressPlatform();
 
-/* implement a database using the firefly database interface */
-class CustomDatabase extends Database {
-    
-    async connect(): Promise<CustomDatabase> {
-        super.displayConnectionMessage();
-        return this;
-    }
-
-    isConnected(): boolean {
-        return false;
-    }
-}
-
-const database = new CustomDatabase();
+/* setup the database and global plugins */
+const database = new MongooseDatabase();
 
 /* start the application */
 new Application({ platform, database }).listen();
