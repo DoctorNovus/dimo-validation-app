075df0404afb372b20ab6c641e8651ba9b726697
hrvdm <harvey@hvdm.io>
2025-03-27 16:59:31 +0000

    wip

--- app/src/_components/Navigation/NavBar.tsx
+++ app/src/_components/Navigation/NavBar.tsx
@@ -3,16 +3,11 @@ import UnitSelector from "./UnitSelector";
 
 export default function NavBar() {
     return (
-        <div className="hidden md:flex w-full h-20 border-b border-b-gray-500/45 justify-between items-center px-4">
-            <div>
-
-            </div>
-
-            <div className="w-1/4 flex justify-center items-center gap-4">
+        <div className="hidden md:flex w-full h-20 border-b border-b-gray-500/45 justify-end">
+            <div className="flex justify-center items-center gap-4 space-x-2 mr-4">
                 <UnitSelector />
 
                 <DimoLogin />
-
             </div>
         </div>
     )
